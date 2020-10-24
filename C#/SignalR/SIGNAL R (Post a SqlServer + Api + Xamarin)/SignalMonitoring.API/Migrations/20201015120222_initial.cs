3ee2e50be74572164d52d41ae02531755b997f52
filda1 <38835920+filda1@users.noreply.github.com>
2020-10-24 23:13:01 +0100

    ...

---
+++ C#/SignalR/SIGNAL R (Post a SqlServer + Api + Xamarin)/SignalMonitoring.API/Migrations/20201015120222_initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SignalMonitoring.API.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Signals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: true),
+                    CustomerName = table.Column<string>(nullable: true),
+                    AccessCode = table.Column<string>(nullable: true),
+                    Area = table.Column<string>(nullable: true),
+                    Zone = table.Column<string>(nullable: true),
+                    SignalDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Signals", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Signals");
+        }
+    }
+}
---
+++ C#/SignalR/SIGNAL R (Post a SqlServer + Api + Xamarin)/SignalMonitoring.API/Services/ISignalService.cs
@@ -0,0 +1,10 @@
+﻿using SignalMonitoring.API.Models;
+using System.Threading.Tasks;
+
+namespace SignalMonitoring.API.Services
+{
+    public interface ISignalService
+    {
+        Task<bool> SaveSignalAsync(SignalInputModel inputModel);
+    }
+}
